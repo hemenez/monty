4ae795331591524694bdee07fe0f7906f5113957
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-20 22:49:01 +0000

    Removed helper functions from handler fxn

--- file_handler.c
+++ file_handler.c
@@ -35,38 +35,3 @@ void file_handler (const char *filename)
 	fclose(fp);
 	free(buffer)
 }
-
-void pushfxn(stack_t **stack, unsigned int number)
-{
-	stack_t new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-	{
-		//return an error
-	}
-	new_node->n = number;
-	new_node->prev = NULL;
-	new_node->next = *stack;
-	*stack = new_node;
-}
-
-void pallfxn(stack_t **stack, unsigned int number)
-{
-	stack_t *printer;
-	int i;
-
-	printer = *stack;
-	i = 0;
-	while (printer != NULL)
-	{
-		printf("%d\n", printer->n);
-		printer = printer->next;
-		i++;
-	}
-	return (i);
-}
-
-void pint(stack_t **stack, unsigned int number)
-{
-	if (*stack != NULL)
-		printf("%d\n", *stack->n);
-}
