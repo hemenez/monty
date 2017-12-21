102ec0a24b2e3e50ba68e6c1b75400c406fe6443
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-21 07:41:56 +0000

    Declared external variable at beginning of file

--- pintfxn.c
+++ pintfxn.c
@@ -1,13 +1,15 @@
 #include "monty.h"
+int op_int;
 /**
- *
- *
- *
- *
+ * pintfxn - function prints the value at the top of the stack
+ * @stack: represents double pointer to beginning of linked list
+ * @line_number: represents line number of matching opcode
+ * Return: function is void
  */
 
-void pintfxn(stack_t **stack, unsigned int number)
+void pintfxn(stack_t **stack, unsigned int line_number)
 {
-	if (*stack != NULL)
-		printf("%d\n", *stack->n);
+	(void) line_number;
+	if (stack != NULL)
+		printf("%d\n", (*stack)->n);
 }
