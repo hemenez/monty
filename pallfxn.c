c68a83b2dcd70d7421959ef4338a4059ffc7c6bb
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-21 07:41:42 +0000

    Declared external variable at beginning of file

--- pallfxn.c
+++ pallfxn.c
@@ -1,21 +1,21 @@
 #include "monty.h"
+int op_int;
 /**
- *
- *
- *
+ * pallfxn - function prints all the values on the stack
+ * @stack: represents double pointer to start of linked list
+ * @line_number: represents line number of opcode
+ * Return: function is void
  */
 
-void pallfxn(stack_t **stack, unsigned int number)
+void pallfxn(stack_t **stack, unsigned int line_number)
 {
 	stack_t *printer;
-	int i;
+	(void) line_number;
 
 	printer = *stack;
-	i = 0;
 	while (printer != NULL)
 	{
 		printf("%d\n", printer->n);
 		printer = printer->next;
-		i++;
 	}
 }
